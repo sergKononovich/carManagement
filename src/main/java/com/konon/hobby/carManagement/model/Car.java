2291582982afaf2d72ddbf38ef5cc8ec44ffecd9
sergeykon <beergood6@gmail.com>
2020-09-24 21:20:49 +0300

    Инициализация проекта.

---
+++ src/main/java/com/konon/hobby/carManagement/model/Car.java
@@ -0,0 +1,24 @@
+package com.konon.hobby.carManagement.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Car {
+
+    public Car() {
+
+    }
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String number;
+    private String name;
+    private String description;
+}
